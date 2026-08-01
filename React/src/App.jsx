936f33b0124e84978466c0496713268d93378b04
priyanshu8184 <priyanshu74164@gmail.com>
2026-08-01 23:00:01 +0530

    first commit

---
+++ React/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <div>
+        <h1>Welcome to HealPoint⚕️</h1>
+        <p>Your health is our priority.</p>
+      </div>
+    </div>
+  )
+}
+
+export default App
