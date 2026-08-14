6efc183324c57daeafbab16a7ece7bf5d4018e09
priyanshu8184 <priyanshu74164@gmail.com>
2026-08-14 23:53:09 +0530

    chore: remove backend directory from main branch

--- ExpressJs/routes/messageRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getMessagesByAppointmentId } = require('../controllers/messageController');
-
-router.get('/:appointmentId', getMessagesByAppointmentId);
-
-module.exports = router;
